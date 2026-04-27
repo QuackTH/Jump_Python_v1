e19065db60c6a6767f13672706396eaba4b33c8d
QuackTH <cheatp24@gmail.com>
2026-04-28 00:02:47 +0900

    함수 내용 추가

--- Python/JumpPy/함수.py
+++ Python/JumpPy/함수.py
@@ -1,4 +1,26 @@
-def add(a, b): # parametter
-    return a + b
+# def add(a, b): # parametter
+#     return a + b
 
-print(add(3, 4)) # arguments
+# print(add(3, 4)) # arguments
+
+
+
+# # add함수
+# a = 3
+# b = 4
+# c = add(a, b)
+# print(c)
+
+
+# # 매개변수와 인수
+# # 매개변수와 인수는 혼용해서 사용하는 용어이므로, 잘 기억해두자. 
+# """매개변수는 함수에 입력으로 전달 된 값을 전달 받는 변수, 인수는 함수를 호출할 때 전달하는 입력갑을 의미한다"""
+
+
+
+# 일반적인 함수
+# 입력값이 있고 리턴값이 있는 함수가 일반적인 함수
+
+def add(a, b):
+    result = a + b
+    return result
